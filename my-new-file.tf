612263034086012f0f8f5b6d7383c65de12175f2
ArmelB1 <barmelmay2020@yahoo.com>
2022-05-25 21:28:55 -0400

    change form salve to dterra

--- my-new-file.tf
+++ my-new-file.tf
@@ -20,3 +20,12 @@ resource "aws_subnet" "my_pub" {
   }
 }
 
+resource "aws_subnet" "my_prv" {
+  vpc_id = aws_vpc.my_vpc.id  
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true 
+  tags = {
+    Name = "my_prv1"
+  }
+}
